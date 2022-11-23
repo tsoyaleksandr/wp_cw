9ec3ef41cf93cc5359a9d3245c431129a3cbc381
tsoyaleksandr <wiut00014432@gmail.com>
2022-11-23 22:17:04 +0500

    contacts page added, all pages updated 23.11.2022

--- branches.js
+++
@@ -1,7 +0,0 @@
-function initMap() {
-  var location = {lat: 41.342891300483366, lng: 69.26480680194942 };
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: location
-  });
-}
